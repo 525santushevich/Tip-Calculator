7f79c03aa467b2c7b984cdb6c02f3a6b981ad783
525santushevich <sophiaantushevich@icloud.com>
2024-01-22 09:31:17 -0500

    edited java

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let bill = 120
+function calculateAndDisplay()
+{
+    //Get input vaues from html forms
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseFloat(document.getElementById('numOfPeople').value)
+    let serviceQuality = parseFloat(document.getElementById('serviceQuality').value)
+
+    //Calculate tip, total bill, and amount per person with helper functions
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calculateAmtPerPrs(totalBill, numOfPpl)
+
+    //Display results in html document
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText =
+        'Total: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText =
+        'Amount Per Person: $' + amtPerPers.toFixed(2)
+
+}
+
+
+
+
+/* let bill = 120
 let numOfPpl = 5
 let serviceQuality = 'Great'
 
@@ -30,4 +54,4 @@ let amtPerPers = calcAmtPerPrs(totalBill, numOfPpl)
 
 console.log('Tip: $' + tip)
 console.log('Total Bill: S' + totalBill)
-console.log('Amount per prson: $' + amtPerPers)
+console.log('Amount per prson: $' + amtPerPers) */

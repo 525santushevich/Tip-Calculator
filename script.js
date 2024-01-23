ffa9b83bd5b892f248437b428946166cc201e035
525santushevich <sophiaantushevich@icloud.com>
2024-01-23 10:40:05 -0500

    final changes to java

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function calculateAndDisplay()
-{
+function calculateAndDisplay() {
     //Get input vaues from html forms
     let bill = parseFloat(document.getElementById('totalBill').value)
     let numOfPpl = parseFloat(document.getElementById('numOfPeople').value)
-    let serviceQuality = parseFloat(document.getElementById('serviceQuality').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
 
     //Calculate tip, total bill, and amount per person with helper functions
     let tip = calculateTip(bill, serviceQuality)
     let totalBill = calculateTotalBill(bill, tip)
-    let amtPerPers = calculateAmtPerPrs(totalBill, numOfPpl)
+    let amtPerPers = calculateAmtPerPers(totalBill, numOfPpl)
 
     //Display results in html document
     document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
@@ -16,42 +15,27 @@ function calculateAndDisplay()
         'Total: $' + totalBill.toFixed(2)
     document.getElementById('amtPerPersonResult').innerText =
         'Amount Per Person: $' + amtPerPers.toFixed(2)
-
 }
 
-
-
-
-/* let bill = 120
-let numOfPpl = 5
-let serviceQuality = 'Great'
-
-function calculateTip(quality) {
-    if(quality === 'Great'){
+//Helper function to calculate tip based on bill and service quality
+function calculateTip(bill, quality) {
+    if(quality === 'Great') {
         return bill * .20
-    } else if (quality === 'Good'){
+    } else if (quality === 'Good') {
         return bill * .15
-    }else if (quality === 'Poor'){
+    } else if (quality === 'Poor') {
         return bill * .10
-    }else {
-        //alert("Please enter a valid value")
-    }
+    } else {
+        return 0
+  }
 }
 
-let tip = calculateTip(serviceQuality)
-
-function calculateTotalBill(bill, tip){
+//Helper function to calculate total bill by adding orginal bill and tip
+function calculateTotalBill(bill, tip) {
     return bill + tip
 }
 
-let totalBill = calculateTotalBill(bill, tip)
-
-function calcAmtPerPrs(TotalBill, numOfPpl){
+//Helper function to calculate amount per person by dividing total bill by number of people
+function calculateAmtPerPers(totalBill, numOfPpl) {
     return totalBill / numOfPpl
 }
-
-let amtPerPers = calcAmtPerPrs(totalBill, numOfPpl)
-
-console.log('Tip: $' + tip)
-console.log('Total Bill: S' + totalBill)
-console.log('Amount per prson: $' + amtPerPers) */
